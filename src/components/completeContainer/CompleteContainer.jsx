dae04f47cd9cf71338139e54252e21aa47263cbc
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-11-18 18:50:49 +0000

    changed the title of the webpage, also added a font family which led to some necessary changes in the fontsize

--- src/components/completeContainer/CompleteContainer.jsx
+++ src/components/completeContainer/CompleteContainer.jsx
@@ -4,7 +4,10 @@ function CompleteContainer() {
   return (
     <div className="complete-container" hidden>
       <h1 className="complete-title">countdown complete</h1>
-      <h1>countdown finished on 05-05-2020</h1>
+      <h1>
+        countdown finished on
+        <br /> 05-05-2020
+      </h1>
       <button className="new-countdown">New countdown</button>
     </div>
   );
